a72e99a3a304b58e6bb9105667f800ae1c4e6847
csaszarattila42 <csaszarattila90@gmail.com>
2022-05-04 17:51:44 +0200

    refactor: Move login managing functions to its own module

---
+++ static/js/controller/loginManager.js
@@ -0,0 +1,35 @@
+import {domManager} from "../view/domManager.js";
+import {dataHandler} from "../data/dataHandler.js";
+
+export const loginManager = {
+    initLoginLogoutHandlers: function() {
+        //domManager.addEventListener("#register-modal-button", "click", registrationHandler);
+        domManager.addEventListener("#login-modal-button", "click", loginHandler);
+
+        if (domManager.isUserLoggedIn()) {
+        domManager.switchToLoggedOut();
+        } else {
+            domManager.switchToLoggedIn(logOutHandler)
+        }
+    }
+}
+
+function loginHandler(clickEvent) {
+    const userName = document.querySelector("#login-modal-username-text").value;
+    const password = document.querySelector("#login-modal-password-text").value;
+
+    dataHandler.logUserIn(userName, password).then((data) => {
+        if (data["result"] === "successful") {
+            domManager.switchToLoggedIn(logOutHandler, userName);
+            domManager.refreshPage();
+        }
+    });
+}
+
+function registrationHandler(clickEvent) {
+    ;
+}
+
+export function logOutHandler(clickEvent) {
+    ;
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,10 @@
 import {boardsManager} from "./controller/boardsManager.js";
-import {dataHandler} from "./data/dataHandler.js";
-import {domManager} from "./view/domManager.js";
+import {loginManager} from "./controller/loginManager.js";
 
 function init() {
     boardsManager.loadBoards();
     boardsManager.initNewItemEventHandlers();
-
-    initLoginLogoutHandlers();
-    if (domManager.isUserLoggedIn()) {
-        domManager.switchToLoggedOut();
-    } else {
-        domManager.switchToLoggedIn(logOutHandler)
-    }
-}
-
-function initLoginLogoutHandlers() {
-    //domManager.addEventListener("#register-modal-button", "click", registrationHandler);
-    domManager.addEventListener("#login-modal-button", "click", loginHandler);
-}
-
-function loginHandler(clickEvent) {
-    const userName = document.querySelector("#login-modal-username-text").value;
-    const password = document.querySelector("#login-modal-password-text").value;
-
-    dataHandler.logUserIn(userName, password).then((data) => {
-        if (data["result"] === "successful") {
-            domManager.switchToLoggedIn(userName, logOutHandler);
-            domManager.refreshPage();
-        }
-    })
-}
-
-function registrationHandler(clickEvent) {
-    ;
-}
-
-function logOutHandler(clickEvent) {
-    ;
+    loginManager.initLoginLogoutHandlers();
 }
 
 init();
