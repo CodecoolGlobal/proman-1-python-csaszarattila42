72f1ae6dcfe76f876648d1e211b31adc120c641a
csaszarattila42 <csaszarattila90@gmail.com>
2022-05-04 11:27:06 +0200

    feat: Implement login

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,32 @@
 import {boardsManager} from "./controller/boardsManager.js";
+import {dataHandler} from "./data/dataHandler.js";
+import {domManager} from "./view/domManager";
 
 function init() {
     boardsManager.loadBoards();
     boardsManager.initNewItemEventHandlers();
+
+    domManager.switchToLoggedOut(loginHandler, registrationHandler);
+}
+
+function loginHandler(clickEvent) {
+    const userName = document.querySelector("#login-modal-username-text").value;
+    const password = document.querySelector("#login-modal-password-text").value;
+
+    dataHandler.logUserIn(userName, password).then((data) => {
+        if (data["result"] === "successful") {
+            domManager.switchToLoggedIn(userName, logOutHandler);
+            domManager.refreshPage();
+        }
+    })
+}
+
+function registrationHandler(clickEvent) {
+    ;
+}
+
+function logOutHandler(clickEvent) {
+    ;
 }
 
 init();
